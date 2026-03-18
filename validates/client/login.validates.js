12faf7bab95454b1bb823eacdd6a042d259d4011
phamson1928 <pson4282@gmail.com>
2026-03-19 00:22:13 +0700

    update login validate

--- validates/client/login.validates.js
+++ validates/client/login.validates.js
@@ -9,9 +9,4 @@ module.exports.loginValidates = [
 
     body("password")
         .notEmpty().withMessage("Mật khẩu không được để trống")
-        .isLength({ min: 6 }).withMessage("Mật khẩu phải có ít nhất 6 ký tự")
-        .matches(/[A-Z]/).withMessage("Mật khẩu phải có ít nhất 1 chữ in hoa")
-        .matches(/[a-z]/).withMessage("Mật khẩu phải có ít nhất 1 chữ thường")
-        .matches(/[0-9]/).withMessage("Mật khẩu phải có ít nhất 1 chữ số")
-        .matches(/[^A-Za-z0-9]/).withMessage("Mật khẩu phải có ít nhất 1 ký tự đặc biệt")
 ];
